f7e012c0b6947f2b4bf75bc9028fcc4e0afc2a61
Luis M. Gallardo D <lgallard@gmail.com>
2021-10-25 19:51:14 -0300

    Change to string comparision in Terratests

--- tests/fixture/outputs.tf
+++ tests/fixture/outputs.tf
@@ -3,5 +3,5 @@
 #
 output "budget_sns_topic_arn" {
   description = "List of SNS Topic ARNs to be subscribed to in order to delivery the budget billing notifications"
-  value       = module.billing_cloudwatch_alert.sns_topic_arns
+  value       = module.billing_cloudwatch_alert.sns_topic_arns[0]
 }
